0fb2ece33fec5b38694d2d553c042e9cf8f7a75c
Vinicius De Alcantra <viniciusalcantarabarros@gmail.com>
2020-09-23 17:34:27 -0300

    Adicionando cláusula  Try Catch no get Result

--- curso-javascript-projeto-calculadora-clone-master/scripts/controller/CalcController.js
+++ curso-javascript-projeto-calculadora-clone-master/scripts/controller/CalcController.js
@@ -188,7 +188,13 @@ class CalcController{
     }
 
     getResult(){
-        return eval(this._operation.join(""));
+        try{
+            return eval(this._operation.join(""));
+        }catch(e){
+            setTimeout(() =>{
+                this.setError();
+            })
+        }        
     }
 
     calc(){

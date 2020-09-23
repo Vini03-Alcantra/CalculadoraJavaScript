30b5b76fc28b0ac1e3b6bb4f1e7c3c81c45a0497
Vinicius De Alcantra <viniciusalcantarabarros@gmail.com>
2020-09-23 16:28:09 -0300

    Adicionando som a calculadora

--- curso-javascript-projeto-calculadora-clone-master/scripts/controller/CalcController.js
+++ curso-javascript-projeto-calculadora-clone-master/scripts/controller/CalcController.js
@@ -2,6 +2,8 @@ class CalcController{
 
     constructor(){
 
+        this._audio = new Audio('click.mp3');
+        this._audioOnOff = false;
         this._lastNumber = '';
         this._lastOperator = '';
 
@@ -15,6 +17,28 @@ class CalcController{
         this.initialize();
         this.initButtonsEvents();
         this.initKeyboard();
+        this.copyToClipboard();
+    }
+
+    pasteFromClipboard(){
+        document.addEventListener('paste', e=>{
+            let text = e.clipboardData.getData('Text');
+            this.displayCalc = parseFloat(text);
+        });
+    }
+
+    copyToClipboard(){
+        let input = document.createElement("input");
+
+        input.value = this.displayCalc;
+
+        document.body.appendChild(input);
+
+        input.select();
+
+        document.execCommand("Copy");
+
+        input.remove();
     }
 
     initialize(){
@@ -24,11 +48,29 @@ class CalcController{
         }, 1000)                
 
         this.setLastNumberToDisplay()
+        this.pasteFromClipboard();
+
+        document.querySelectorAll(".btn-ac").forEach(btn=>{
+            btn.addEventListener('dblclick', e=>{
+                this.toggleAudio();
+            })
+        })
+    }
+
+    toggleAudio(){
+        this._audioOnOff = !this._audioOnOff;
+    }
+
+    playAudio(){
+        if (this._audioOnOff) {
+            this._audio.currentTime = 0;
+            this._audio.play();
+        }
     }
 
     initKeyboard(){
-        document.addEventListener('keyup', e=>{            
-            console.log(e.key)
+        document.addEventListener('keyup', e=>{     
+            this.playAudio()                   ;
             switch (e.key){
                 case 'Escape':
                     this.clearAll();
@@ -63,7 +105,11 @@ class CalcController{
                     case '8':
                     case '9':
                         this.addOperation(parseInt(e.key));
-                        break;                
+                        break;       
+                    
+                    case 'c':
+                        if(e.ctrlKey) this.copyToClipboard();
+                        break;
                 }        
         })
     }
@@ -217,6 +263,9 @@ class CalcController{
     }
 
     execBtn(value){
+
+        this.playAudio();
+
         switch (value){
         case 'ac':
             this.clearAll();
